f6a770e89bee855446b0bb13c6a3376aa37400e2
viviannakann <viviannakan@gamil.com>
2020-09-01 18:12:22 +0800

    Updated to connect to codecov

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
     clearMocks: true,
     collectCoverageFrom: ['calculator.js'], // here you can configure which folders or files jest will consider coverage
     coverageDirectory: 'coverage',
+	collectCoverage: true,
     testEnvironment: 'node',
     testMatch: ['<rootDir>/__tests__/**/*.js'],
     verbose: true
